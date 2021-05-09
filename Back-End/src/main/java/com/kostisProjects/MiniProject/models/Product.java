29dd1b3e2283d60d73cc079f3bc999a9b7adb908
abc <kostas.messanakis@gmail.com>
2021-05-09 23:56:56 +0300

    Added data lombok annotation

--- Back-End/src/main/java/com/kostisProjects/MiniProject/models/Product.java
+++ Back-End/src/main/java/com/kostisProjects/MiniProject/models/Product.java
@@ -2,7 +2,9 @@ package com.kostisProjects.MiniProject.models;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import lombok.Data;
 
+@Data
 @Document(collection = "products")
 public class Product {
 

cc54df75d3c136c450ee655bf68433f90b0a22b1
abc <kostas.messanakis@gmail.com>
2021-05-10 00:17:45 +0300

    Lombok refactoring of product class

--- Back-End/src/main/java/com/kostisProjects/MiniProject/models/Product.java
+++ Back-End/src/main/java/com/kostisProjects/MiniProject/models/Product.java
@@ -1,11 +1,15 @@
 package com.kostisProjects.MiniProject.models;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import lombok.Data;
 
-//Data model
+//Data model class specifies properties of product
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Document(collection = "products")
 public class Product {
 
@@ -27,78 +31,5 @@ public class Product {
 
     @JsonProperty("asin")
     private String asin;
-
-    public Product() {
-    }
-
-    public Product(String id,String title, String brand, String category, String price) {
-        this.id = id;
-        this.category=category;
-        this.title = title;
-        this.price = price;
-        this.brand = brand;
-        //this.description = description;
-        this.asin=asin;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getPrice() {
-        return price;
-    }
-
-    public void setPrice(String price) {
-        this.price = price;
-    }
-
-    public String getBrand() {
-        return brand;
-    }
-
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
-
-    public String getAsin() {
-        return asin;
-    }
-
-    public void setAsin(String asin) {
-        this.asin = asin;
-    }
-
-    @Override
-    public String toString() {
-        return "Product{" +
-                "id='" + id + '\'' +
-                ", title='" + title + '\'' +
-                ", price='" + price + '\'' +
-                ", brand='" + brand + '\'' +
-                //", description='" + description + '\'' +
-                ", category='" + category + '\'' +
-                ", asin='" + asin + '\'' +
-                '}';
-    }
+    
 }
